7d1bbf51c6735eb3f7b00b9c60366f1a171ee6b2
MarianneHolbie <112870394+MarianneHolbie@users.noreply.github.com>
2024-10-12 15:34:40 +0200

    simulate with mock data retrieve of google API and return restaurants informations

--- MealsToGo/App.js
+++ MealsToGo/App.js
@@ -13,6 +13,7 @@ import {RestaurantsScreen} from "./src/features/restaurants/screens/restaurants.
 import { SafeArea } from "./src/components/utility/safe-area.component";
 import { Ionicons } from "@expo/vector-icons";
 
+import { RestaurantsContextProvider} from "./src/services/restaurants/restaurants.context";
 
 const Tab = createBottomTabNavigator();
 
@@ -50,19 +51,21 @@ export default function App() {
   return (
       <>
           <ThemeProvider theme={theme}>
-            <NavigationContainer>
-                <Tab.Navigator
-                    screenOptions={createScreenOptions}
-                    tabBarOptions={{
-                        activeTintColor: "tomato",
-                        inactiveTintColor: "gray",
-                    }}
-                >
-                    <Tab.Screen name="Restaurants" component={RestaurantsScreen} />
-                    <Tab.Screen name="Map" component={Map} />
-                    <Tab.Screen name="Settings" component={Settings} />
-                </Tab.Navigator>
-            </NavigationContainer>
+              <RestaurantsContextProvider>
+                <NavigationContainer>
+                    <Tab.Navigator
+                        screenOptions={createScreenOptions}
+                        tabBarOptions={{
+                            activeTintColor: "tomato",
+                            inactiveTintColor: "gray",
+                        }}
+                    >
+                        <Tab.Screen name="Restaurants" component={RestaurantsScreen} />
+                        <Tab.Screen name="Map" component={Map} />
+                        <Tab.Screen name="Settings" component={Settings} />
+                    </Tab.Navigator>
+                </NavigationContainer>
+              </RestaurantsContextProvider>
           </ThemeProvider>
         <ExpoStatusBar style="auto" />
       </>
